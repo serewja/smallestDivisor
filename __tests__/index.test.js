d0c08f2880ba77392111ca3d731337a868099538
Sergei Shinkarenko <serewja@gmail.com>
2020-07-11 21:40:11 +0500

    add tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,5 +1,6 @@
-import f from "../dist/index.js"
+import smallestDivisor from "../dist/index.js"
 
-test('testname', () => {
-    
+test('test', () => {
+  expect(smallestDivisor(15)).toBe(3);
+  expect(smallestDivisor(17)).toBe(17);
 });
